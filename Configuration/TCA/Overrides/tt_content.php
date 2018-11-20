b27da771b5efc8e1d6eb339a1f377509713096c0
Stefan Froemken <froemken@gmail.com>
2018-11-20 16:27:12 +0100

    Move all TCA modifying methods into Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,20 +3,41 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function() {
-    $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase('maps2');
-    $pluginSignature = strtolower($extensionName) . '_maps2';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . 'maps2' . '/Configuration/FlexForms/Maps2.xml');
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['maps2_maps2'] = 'select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['maps2_maps2'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'maps2_maps2',
+    'FILE:EXT:maps2/Configuration/FlexForms/Maps2.xml'
+);
 
-    $pluginSignature = strtolower($extensionName) . '_searchwithinradius';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . 'maps2' . '/Configuration/FlexForms/Radius.xml');
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['maps2_searchwithinradius'] = 'select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['maps2_searchwithinradius'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'maps2_searchwithinradius',
+    'FILE:EXT:maps2/Configuration/FlexForms/Radius.xml'
+);
 
-    $pluginSignature = strtolower($extensionName) . '_citymap';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . 'maps2' . '/Configuration/FlexForms/CityMap.xml');
-});
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['maps2_citymap'] = 'select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['maps2_citymap'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'maps2_citymap',
+    'FILE:EXT:maps2/Configuration/FlexForms/CityMap.xml'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.maps2',
+    'Maps2',
+    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.maps.title'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.maps2',
+    'SearchWithinRadius',
+    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.radius.title'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.maps2',
+    'CityMap',
+    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.cityMap.title'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -3,27 +3,10 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'Maps2',
-    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.maps.title'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_maps2_domain_model_poicollection',
+    'EXT:maps2/Resources/Private/Language/locallang_csh_tx_maps2_domain_model_poicollection.xlf'
 );
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'SearchWithinRadius',
-    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.radius.title'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'CityMap',
-    'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:plugin.cityMap.title'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Maps2');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_maps2_domain_model_poicollection', 'EXT:maps2/Resources/Private/Language/locallang_csh_tx_maps2_domain_model_poicollection.xlf');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_maps2_domain_model_poicollection');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_maps2_domain_model_poi');
