4540558eb7817be2c43ef7d66347e0ac2af33a35
Stefan Froemken <froemken@gmail.com>
2019-01-24 16:50:10 +0100

    Use Maps2Registry to add column to tt_address

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -5,27 +5,19 @@ if (!defined('TYPO3_MODE')) {
 
 call_user_func(function() {
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_address')) {
-        $tempColumns = [
-            'tx_maps2_uid' => [
-                'exclude' => 1,
-                'label' => 'LLL:EXT:maps2/Resources/Private/Language/locallang_db.xlf:tx_maps2_uid',
-                'config' => [
-                    'type' => 'group',
-                    'internal_type' => 'db',
-                    'allowed' => 'tx_maps2_domain_model_poicollection',
-                    'prepend_tname' => false,
-                    'size' => 1,
-                    'maxitems' => 1,
-                    'suggestOptions' => [
-                        'default' => [
-                            'searchWholePhrase' => true
-                        ]
+        \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
+            'tt_address',
+            'tt_address',
+            [
+                'addressColumns' => ['address', 'zip', 'city'],
+                'countryColumn' => 'country',
+                'synchronizeColumns' => [
+                    [
+                        'foreignColumnName' => 'name',
+                        'poiCollectionColumnName' => 'title'
                     ]
                 ]
             ]
-        ];
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_address', $tempColumns);
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_address', 'tx_maps2_uid');
+        );
     }
 });
