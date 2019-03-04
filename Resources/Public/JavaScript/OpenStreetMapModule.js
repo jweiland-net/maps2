eb039616eada66d13d180cbbcab462fa57af413b
Stefan Froemken <froemken@gmail.com>
2019-03-04 14:45:31 +0100

    Reload/Refresh map while clicking on second tab in BE

--- Resources/Public/JavaScript/OpenStreetMapModule.js
+++ Resources/Public/JavaScript/OpenStreetMapModule.js
@@ -192,7 +192,7 @@ define("TYPO3/CMS/Maps2/OpenStreetMapModule", ["jquery", "leaflet"], function($,
         }
 
         // if maps2 was inserted in (bootstrap) tabs, we have to re-render the map
-        $("ul.t3js-tabs a[data-toggle='tab']:first").on("shown.bs.tab", function() {
+        $("ul.t3js-tabs a[data-toggle='tab']:eq(1)").on("shown.bs.tab", function() {
             map.invalidateSize();
             if (config.latitude && config.longitude) {
                 map.panTo([config.latitude, config.longitude]);
