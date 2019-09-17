8cc5849e130d937687810409e0349af61a8e4647
Stefan Froemken <froemken@gmail.com>
2019-09-17 14:30:28 +0200

    Add new VH namespacing HTML Header

---
+++ Classes/Utility/CacheUtility.php
@@ -0,0 +1,41 @@
+<?php
+namespace JWeiland\Maps2\Utility;
+
+/*
+ * This file is part of the maps2 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * A little helper to organize our DB queries
+ */
+class DatabaseUtility
+{
+    /**
+     * Get column definitions from table
+     *
+     * @param string $tableName
+     * @return array
+     */
+    public static function getColumnsFromTable($tableName)
+    {
+        $output = [];
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable($tableName);
+        $statement = $connection->query('SHOW FULL COLUMNS FROM `' . $tableName . '`');
+        while ($fieldRow = $statement->fetch()) {
+            $output[$fieldRow['Field']] = $fieldRow;
+        }
+        return $output;
+    }
+}
