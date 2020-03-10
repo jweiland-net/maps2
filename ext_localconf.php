6663d24a301405a9301983696a0852c5fdb7d501
Stefan Froemken <froemken@gmail.com>
2020-03-10 13:27:12 +0100

    Remove Maps2Registry Cache from ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -47,13 +47,6 @@ call_user_func(function() {
         ];
     }
 
-    // Register cache to store registry entries of foreign extensions
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maps2_registry'] = [
-        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-        'backend' => \TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend::class,
-        'groups' => ['all', 'system']
-    ];
-
     // This is a solution to build GET forms.
     $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_maps2_citymap[street]';
     // Create maps2 records while saving foreign records
