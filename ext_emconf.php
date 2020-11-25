da161eb0e7a479958cbba4802fa922f7f107978d
Stefan Froemken <froemken@gmail.com>
2020-11-25 14:49:56 +0100

    Update version to 9.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Maps2',
     'description' => 'Create maps with Marker, Area, Routes or Radius based on Google Maps or OpenStreetMap',
-    'version' => '9.2.1',
+    'version' => '9.3.0',
     'category' => 'plugin',
     'state' => 'stable',
     'uploadfolder' => false,
