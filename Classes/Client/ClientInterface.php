6d502a7da3080df7a802f98a315b2a803c29f115
Stefan Froemken <froemken@gmail.com>
2020-12-01 17:15:13 +0100

    Add getErrors/hasErrors into Client Interface

--- Classes/Client/ClientInterface.php
+++ Classes/Client/ClientInterface.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace JWeiland\Maps2\Client;
 
 use JWeiland\Maps2\Client\Request\RequestInterface;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
 
 /**
  * Interface for Maps2 Clients
@@ -24,4 +25,5 @@ interface ClientInterface
      * @throws \Exception
      */
     public function processRequest(RequestInterface $request);
+
 }
