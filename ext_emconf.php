6d5ed4a221ee01910dbb99ea51e0f45df3914148
Stefan Froemken <froemken@gmail.com>
2022-03-11 15:39:07 +0100

    Update version to 10.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Maps2',
     'description' => 'Create maps with Marker, Area, Routes or Radius based on Google Maps or OpenStreetMap',
-    'version' => '10.0.1',
+    'version' => '10.0.2',
     'category' => 'plugin',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
