0981fdf038859f4d603287d6165504a5f251978b
Stefan Froemken <sfroemken@jweiland.net>
2023-10-16 10:58:27 +0200

    Remove internal_type from Maps2Registry

--- Classes/Tca/Maps2Registry.php
+++ Classes/Tca/Maps2Registry.php
@@ -344,10 +344,8 @@ class Maps2Registry implements SingletonInterface
     public static function getTcaFieldConfiguration(array $fieldConfigurationOverride = []): array
     {
         // Forges a new field, default name is "categories"
-        // ToDo: Remove "internal_type" while removing TYPO3 11 compatibility
         $fieldConfiguration = [
             'type' => 'group',
-            'internal_type' => 'db',
             'allowed' => 'tx_maps2_domain_model_poicollection',
             'foreign_table' => 'tx_maps2_domain_model_poicollection',
             'prepend_tname' => 0,
