816151be6a22fce4223ab8bdf804118a72a13922
Stefan Froemken <sfroemken@jweiland.net>
2026-05-20 08:53:00 +0200

    [BUGFIX] Exclude 'packages' directory in PHP-CS-Fixer configuration
    Adjust the PHP-CS-Fixer configuration to exclude the 'packages' directory,
    aligning with existing project directory structure and exclusion rules.

--- Build/cgl/config.php
+++ Build/cgl/config.php
@@ -168,7 +168,7 @@ return (new \PhpCsFixer\Config())
     ->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setFinder(
         (new PhpCsFixer\Finder())
-            ->exclude(['var'])
+            ->exclude(['var', 'packages'])
             ->ignoreVCSIgnored(true)
             ->in(__DIR__ . '/../../'),
     )
