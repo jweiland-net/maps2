f0abca7f423770ba7725388db3685d2a4a473192
Stefan Froemken <sfroemken@jweiland.net>
2026-05-27 10:30:01 +0200

    [BUGFIX] Streamline plugin registration in Maps2
    Simplify and optimize the plugin registration process by removing the unnecessary static function wrapper. Improve code readability and maintain consistency with TYPO3 best practices.

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,80 +19,78 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-call_user_func(static function (): void {
-    ExtensionUtility::configurePlugin(
-        'Maps2',
-        'Maps2',
-        [
-            PoiCollectionController::class => 'show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
+ExtensionUtility::configurePlugin(
+    'Maps2',
+    'Maps2',
+    [
+        PoiCollectionController::class => 'show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-    ExtensionUtility::configurePlugin(
-        'Maps2',
-        'Overlay',
-        [
-            PoiCollectionController::class => 'overlay',
-        ],
-        [
-            PoiCollectionController::class => 'overlay',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
+ExtensionUtility::configurePlugin(
+    'Maps2',
+    'Overlay',
+    [
+        PoiCollectionController::class => 'overlay',
+    ],
+    [
+        PoiCollectionController::class => 'overlay',
+    ],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-    ExtensionUtility::configurePlugin(
-        'Maps2',
-        'SearchWithinRadius',
-        [
-            PoiCollectionController::class => 'search, listRadius',
-        ],
-        [
-            PoiCollectionController::class => 'listRadius',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
+ExtensionUtility::configurePlugin(
+    'Maps2',
+    'SearchWithinRadius',
+    [
+        PoiCollectionController::class => 'search, listRadius',
+    ],
+    [
+        PoiCollectionController::class => 'listRadius',
+    ],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-    ExtensionUtility::configurePlugin(
-        'Maps2',
-        'CityMap',
-        [
-            CityMapController::class => 'show, search',
-        ],
-        [
-            CityMapController::class => 'search',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
+ExtensionUtility::configurePlugin(
+    'Maps2',
+    'CityMap',
+    [
+        CityMapController::class => 'show, search',
+    ],
+    [
+        CityMapController::class => 'search',
+    ],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-    // Activate caching for info window content
-    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maps2_cachedhtml'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maps2_cachedhtml'] = [
-            'groups' => ['pages', 'all'],
-        ];
-    }
-
-    // This is a solution to build GET forms.
-    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters']['maps2'] = 'tx_maps2_citymap[street]';
-
-    // Create maps2 records while saving foreign records
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['createMaps2Record']
-        = CreateMaps2RecordHook::class;
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1530778687] = [
-        'nodeName' => 'maps2InfoWindowContent',
-        'priority' => 40,
-        'class' => InfoWindowContent::class,
+// Activate caching for info window content
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maps2_cachedhtml'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maps2_cachedhtml'] = [
+        'groups' => ['pages', 'all'],
     ];
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1633612058] = [
-        'nodeName' => 'maps2ReadOnlyInputText',
-        'priority' => 40,
-        'class' => ReadOnlyInputTextElement::class,
-    ];
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeResolver'][1551448205] = [
-        'nodeName' => 'maps2MapProvider',
-        'priority' => 40,
-        'class' => MapProviderResolver::class,
-    ];
-});
+}
+
+// This is a solution to build GET forms.
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters']['maps2'] = 'tx_maps2_citymap[street]';
+
+// Create maps2 records while saving foreign records
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['createMaps2Record']
+    = CreateMaps2RecordHook::class;
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1530778687] = [
+    'nodeName' => 'maps2InfoWindowContent',
+    'priority' => 40,
+    'class' => InfoWindowContent::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1633612058] = [
+    'nodeName' => 'maps2ReadOnlyInputText',
+    'priority' => 40,
+    'class' => ReadOnlyInputTextElement::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeResolver'][1551448205] = [
+    'nodeName' => 'maps2MapProvider',
+    'priority' => 40,
+    'class' => MapProviderResolver::class,
+];
