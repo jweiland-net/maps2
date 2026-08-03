6d33a7dc2b3ff2666e93695edbb9cc17672856a8
Stefan Froemken <sfroemken@jweiland.net>
2026-08-03 14:52:46 +0200

    [BUGFIX] Bump ext_emconf.php version to 13.0.1
    The previous version bump updated composer.json and
    Documentation/guides.xml but missed the version field in
    ext_emconf.php, leaving it out of sync at 13.0.0.

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Maps2',
     'description' => 'Create maps with Marker, Area, Routes or Radius based on Google Maps or OpenStreetMap',
-    'version' => '13.0.0',
+    'version' => '13.0.1',
     'category' => 'plugin',
     'state' => 'stable',
     'author' => 'Stefan Froemken',
